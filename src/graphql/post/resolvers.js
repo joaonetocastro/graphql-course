9c4d87afea8480afc1d77d12bc9b276d7fbfd8de
João Neto C <j.neto111@gmail.com>
2022-12-28 10:46:29 -0300

    feat: union post

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,6 +1,14 @@
 const post = async (_obj, {id}, {getPosts}) => {
     const response = await getPosts(`/${id}`)
     const result = await response.json()
+
+    if(typeof result.id === 'undefined') {
+        return {
+            statusCode: 404,
+            message: 'Post Not Found'
+        }
+    }
+    
     return result
 }
 
@@ -18,5 +26,12 @@ export const postResolvers = {
     },
     Post: {
         unixTimestamp: ({createdAt}) => Math.floor(new Date(createdAt).getTime() / 1000)
+    },
+    PostResult: {
+        __resolveType: (obj) => {
+            if(typeof obj.statusCode !== 'undefined') return 'PostNotFoundError'
+            if(typeof obj.id !== 'undefined') return 'Post'
+            return null
+        }
     }
 }
--- src/graphql/post/typedefs.js
+++ src/graphql/post/typedefs.js
@@ -2,10 +2,17 @@ import { gql } from "apollo-server";
 
 export const postTypeDefs = gql`
     extend type Query {
-        post(id: ID!): Post
+        post(id: ID!): PostResult
         posts(input: ApiFiltersInput): [Post!]!
     }
 
+    type PostNotFoundError {
+        statusCode: Int!
+        message: String!
+    }
+
+    union PostResult = PostNotFoundError | Post
+    
     type Post {
         id: ID!
         title: String!
